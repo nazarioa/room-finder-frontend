3c7c43a46f966eaf9f77a8be23e9441e246b3eb1
Nazario A. Ayala <nazario@niztech.com>
2018-05-14 18:16:56 -0700

    Added click functionality for rooms

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,33 +6,59 @@ import Rx from 'rxjs/Rx';
   const tplPersonListItem = document.querySelector('#tpl-person-list-item');
   const tplRoomListItem = document.querySelector('#tpl-room-list-item');
 
-  const searchResults = document.querySelector('.search-results');
-  const searchResultList = document.querySelector('.search-results-list');
-
   const selectedResult = document.querySelector('.selected-result');
 
   const inpSearch = document.querySelector('.inp.inp-search');
 
   const SelectedResult = {
-    item: null,
+    room_id: null,
+    building_id: null,
     show: false,
 
-    render: function () {
-      if (this.show) {
-        if (floor && floor.image_url) {
-          selectedResult.querySelector('.result-map img').setAttribute('src', 'floor.image_url');
-        } else {
-          selectedResult.querySelector('.result-map img').setAttribute('src', '//defualtUrl.');
-        }
+    populateRoomData: function (roomId) {
+      this.room_id = roomId;
+      const getRoomData = fetch(`http://api.room-finder.local/Rooms/view/${this.room_id}`)
+        .then(response => response.json())
+        .then(data => data.room)
+        .then(room => {
+          console.log(room);
+          const roomDetailsDisplay = document.querySelector('.room-details');
+          roomDetailsDisplay.querySelector('.floor.value').innerHTML = room.floor.name;
+          roomDetailsDisplay.querySelector('.room.value').innerHTML = room.name;
+          roomDetailsDisplay.querySelector('.roomcode.value').innerHTML = room.code;
+          this.populateBuildingData(room.floor.building_id);
+        });
+    },
 
+    populateBuildingData: function (buildingId) {
+      this.building_id = buildingId;
+      const getBuildingData = fetch(`http://api.room-finder.local/Buildings/view/${this.building_id}`)
+        .then(response => response.json())
+        .then(data => data.building)
+        .then(building => {
+          const buildingDetailsDisplay = document.querySelector('.room-details');
+          buildingDetailsDisplay.querySelector('.building-name .value').innerHTML = building.name;
+          buildingDetailsDisplay.querySelector('.building-address .address.value').innerHTML = building.address;
+          buildingDetailsDisplay.querySelector('.building-address .city.value').innerHTML = building.city;
+          buildingDetailsDisplay.querySelector('.building-address .state.value').innerHTML = building.state;
+        });
+    },
+
+    render: function () {
+      if (!this.show) {
+        selectedResult.classList.add('hidden');
+      } else if (this.room_id) {
         selectedResult.classList.remove('hidden');
       } else {
         selectedResult.classList.add('hidden');
+        // Maybe remove hidden from an error message here
       }
     }
   };
 
   const ResultsList = {
+    searchResultList: document.querySelector('.search-results .search-results-list'),
+
     list: [],
 
     discoverType: function (item) {
@@ -47,9 +73,22 @@ import Rx from 'rxjs/Rx';
       return 'unknown';
     },
 
+    registerLiClick: function () {
+      this.searchResultList.addEventListener('click', event => {
+        const selectedRoomId = event.srcElement.closest('.list-item').dataset.roomId;
+        if (selectedRoomId) {
+          SelectedResult.populateRoomData(selectedRoomId);
+          SelectedResult.show = true;
+        } else {
+          // Maybe show an error that the thing we clicked on does not have a roomId.
+          SelectedResult.show = false;
+        }
+        SelectedResult.render();
+      });
+    },
+
     render: function () {
-      const searchResultList = document.querySelector('.search-results .search-results-list');
-      searchResultList.innerHTML = '';
+      this.searchResultList.innerHTML = '';
 
       this.list.forEach(item => {
         const type = this.discoverType(item);
@@ -58,6 +97,7 @@ import Rx from 'rxjs/Rx';
 
         if (type === 'person') {
           row = document.importNode(tplPersonListItem.content, true);
+          row.querySelector('.person-list-item.list-item').dataset.personId = item.id;
           row.querySelector('.first-name.value').innerHTML = item.first_name;
           row.querySelector('.last-name.value').innerHTML = item.last_name;
           row.querySelector('.email.value').innerHTML = item.email;
@@ -66,6 +106,7 @@ import Rx from 'rxjs/Rx';
 
         if (type === 'room') {
           row = document.importNode(tplRoomListItem.content, true);
+          row.querySelector('.room-list-item.list-item').dataset.roomId = item.id;
           row.querySelector('.room-detail .room.value').innerHTML = item.name || '';
           row.querySelector('.room-detail .floor.value').innerHTML = item.Floors.name || '';
           row.querySelector('.building-detail .building-name.value').innerHTML = item.Buildings.name || '';
@@ -126,5 +167,6 @@ import Rx from 'rxjs/Rx';
   });
 
   ResultsList.render();
+  ResultsList.registerLiClick();
 
 })(window);
